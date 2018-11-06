fe47c69f561851cc975ebe019638f97a8c16281b
Ryan Medlin <ryan.medlin@presencepg.com>
2018-11-06 09:08:06 -0800

    more adjustment of paths

--- deployment/deployGoogleBackend.sh
+++ deployment/deployGoogleBackend.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 
-rsync -avz -e "ssh -o StrictHostKeyChecking=no" .. ryan_medlin@35.236.68.11:~/grip/grip-client@35.236.68.11:/home/ryan_medlin/grip/grip-client
+rsync -avz -e "ssh -o StrictHostKeyChecking=no" .. ryan_medlin@35.236.68.11:/home/ryan_medlin/grip/grip-client
 
-ssh -o StrictHostKeyChecking=no ryan_medlin@35.236.68.11:~/grip/grip-client@35.236.68.11 'cd /home/ryan_medlin/grip/grip-client && git clean -xfd  && cp ~/.grip/.env.integration env.production && docker-compose up --build -d'
+ssh -o StrictHostKeyChecking=no ryan_medlin@35.236.68.11 'cd ~/grip/grip-client && cd /home/ryan_medlin/grip/grip-client && git clean -xfd  && cp ~/.grip/.env.integration env.production && docker-compose up --build -d'
 
 
 
