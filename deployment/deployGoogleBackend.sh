788c5b6cc8675fc1f15acda541c10f77301698ec
Ryan Medlin <ryan.medlin@presencepg.com>
2018-11-07 14:12:52 -0800

    work with very clean folder

--- deployment/deployGoogleBackend.sh
+++ deployment/deployGoogleBackend.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 
-
+ssh -o StrictHostKeyChecking=no ryan_medlin@35.236.68.11 'cd ~/grip/grip-cleint &&  sudo rm -rf *'
 
 rsync -avz -e "ssh -o StrictHostKeyChecking=no" /home/circleci/repo/ ryan_medlin@35.236.68.11:/home/ryan_medlin/grip/grip-client
 
