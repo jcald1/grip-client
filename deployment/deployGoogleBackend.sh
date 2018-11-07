6fd1a2033e39168d9363e54f1870d7c8c7942b47
Ryan Medlin <ryan.medlin@presencepg.com>
2018-11-07 13:37:08 -0800

    resetting directory before rsync

--- deployment/deployGoogleBackend.sh
+++ deployment/deployGoogleBackend.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+ssh -o StrictHostKeyChecking=no ryan_medlin@35.236.68.11 'cd ~/grip/grip-client &&  sudo rm -rf *'
 
 rsync -avz -e "ssh -o StrictHostKeyChecking=no" /home/circleci/repo/ ryan_medlin@35.236.68.11:/home/ryan_medlin/grip/grip-client
 
