908be7ab036c6817a6ccbf81db0f990a08a4eaab
Jorge Calderon <jcald1@gmail.com>
2018-11-26 22:08:21 -0500

    Changed IP address.

--- deployment/deployGoogleBackend.sh
+++ deployment/deployGoogleBackend.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 
-ssh -o StrictHostKeyChecking=no ryan_medlin@35.236.68.11 'cd ~/grip/grip-cleint &&  sudo rm -rf *'
+ssh -o StrictHostKeyChecking=no ryan_medlin@35.236.78.59 'cd ~/grip/grip-cleint &&  sudo rm -rf *'
 
-rsync -avz -e "ssh -o StrictHostKeyChecking=no" /home/circleci/repo/ ryan_medlin@35.236.68.11:/home/ryan_medlin/grip/grip-client
+rsync -avz -e "ssh -o StrictHostKeyChecking=no" /home/circleci/repo/ ryan_medlin@35.236.78.59:/home/ryan_medlin/grip/grip-client
 
-ssh -o StrictHostKeyChecking=no ryan_medlin@35.236.68.11 'cd ~/grip/grip-client && cd /home/ryan_medlin/grip/grip-client && git clean -xfd  && cp ~/.grip/grip-client/.env.integration .env && cp ~/.grip/grip-client/.env.integration .env.production && yes | 
+ssh -o StrictHostKeyChecking=no ryan_medlin@35.236.78.59 'cd ~/grip/grip-client && cd /home/ryan_medlin/grip/grip-client && git clean -xfd  && cp ~/.grip/grip-client/.env.integration .env && cp ~/.grip/grip-client/.env.integration .env.production && yes | 
 docker system prune && docker-compose up --build -d'
 
 
